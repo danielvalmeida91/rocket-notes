e2ef5f504237349ed3c511acb844c4718905aa44
danielvalmeida91 <danielvalmeida91@gmail.com>
2022-09-29 07:57:17 -0300

    First commit

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,19 @@
+class UsersController {
+  /**
+   * index - GET para listar vários registros
+   * show - GET para exibir um registo específico
+   * create - POST para criar um registro
+   * update - PUT para atualizar um registro
+   * delete - DELETE para remover um registro
+   *
+   * Se houver a necessidade de ter mais de 5 métodos é necessário avaliar sua separação em um novo controller.
+   */
+
+  create(request, response) {
+    const { name, email, password } = request.body
+
+    response.status(201).json({ name, email, password })
+  }
+}
+
+module.exports = UsersController
