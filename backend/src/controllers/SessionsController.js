df07af41020a474ca64615a03e05b4d55a045e87
danielvalmeida91 <danielvalmeida91@gmail.com>
2022-12-23 15:44:45 -0300

    Middlewares

--- backend/src/controllers/SessionsController.js
+++ backend/src/controllers/SessionsController.js
@@ -1,6 +1,8 @@
 const knex = require('../database/knex')
 const AppError = require('../utils/AppError')
 const { compare } = require('bcryptjs')
+const { sign } = require('jsonwebtoken')
+const authConfig = require('../configs/auth.js')
 
 class SessionsController {
   async create(request, response) {
@@ -15,10 +17,17 @@ class SessionsController {
     const passwordMatched = await compare(password, user.password)
 
     if (!passwordMatched) {
-      throw new AppError(' E-mail e/ou senha incorreta.', 401)
+      throw new AppError('E-mail e/ou senha incorreta.', 401)
     }
 
-    return response.json(user)
+    const { secret, expiresIn } = authConfig.jwt
+
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn
+    })
+
+    return response.json({ user, token })
   }
 }
 
