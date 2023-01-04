9586420e0a7f2d53eb522374a8bb61a0ee1fb2c3
danielvalmeida91 <danielvalmeida91@gmail.com>
2023-01-03 22:34:15 -0300

    Links and Tags finished

--- frontend/src/pages/New/index.jsx
+++ frontend/src/pages/New/index.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import {  Link } from 'react-router-dom'
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
@@ -9,6 +10,30 @@ import { Button } from '../../components/Button';
 import { Container, Form  } from './styles'
 
 export function New(){
+  const [ links, setLinks ] = useState([])
+  const [ newLink, setNewLink ] = useState("")
+
+  const [ tags, setTags ] = useState([])
+  const [ newTag, setNewTag ] = useState("")
+
+  function handleAddLink(){
+    setLinks(prevState => [...prevState, newLink])
+    setNewLink("")
+  }
+
+  function handleRemoveLink(deleted){
+    setLinks( prevState => prevState.filter(link => link !== deleted))
+  }
+
+  function handleAddTag(){
+    setTags( prevState => [...prevState, newTag])
+    setNewTag("")
+  }
+
+  function handleRemoveTag(deleted){
+    setTags( prevState => prevState.filter(tag => tag !== deleted))
+  }
+
   return(
     <Container>
       <Header />
@@ -25,14 +50,42 @@ export function New(){
           <Textarea placeholder="Observações" />
 
           <Section title="Links úteis">
-            <NoteItem value='https://rocketseat.com.br' />
-            <NoteItem isNew placeholder="Novo link"  />
+            {
+              links.map( (link, index) => (
+                <NoteItem
+                  key={String(index)}
+                  value={link}
+                  onClick={() => handleRemoveLink(link)}
+                />
+              ))
+            }
+            <NoteItem
+              isNew
+              placeholder="Novo link"  
+              value={newLink}
+              onChange={e => setNewLink(e.target.value)}
+              onClick={handleAddLink}
+            />
           </Section>
 
           <Section title="Marcadores">
             <div className='tags'>
-              <NoteItem value="react" />
-              <NoteItem isNew placeholder="Nova tag "  />
+              {
+                tags.map((tag, index) => (
+                  <NoteItem
+                    key={String(index)}
+                    value={tag}
+                    onClick={() => handleRemoveTag(tag)}
+                  />    
+                ))
+              }
+              <NoteItem
+                isNew
+                placeholder="Nova tag"
+                value={newTag}
+                onChange={e => setNewTag(e.target.value)}
+                onClick={handleAddTag}  
+              />
             </div>
           </Section>
 
--- frontend/src/pages/Profile/index.jsx
+++ frontend/src/pages/Profile/index.jsx
@@ -1,10 +1,45 @@
+import { useState } from 'react'
 import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi"
 import { Link } from 'react-router-dom'
+
+import { useAuth } from '../../hooks/auth'
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
+
 import { Container, Form, Avatar} from "./styles";
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
+import { api } from '../../services/api'
 
 export function Profile(){
+  const { user, updateProfile } = useAuth()
+  const [ name, setName ] = useState(user.name)
+  const [ email, setEmail ] = useState(user.email)
+  const [ passwordOld, setPasswordOld] = useState()
+  const [ passwordNew , setPasswordNew ] = useState()
+
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
+  const [ avatar, setAvatar ] = useState(avatarUrl)
+  const [avatarFile, setAvatarFile] = useState(null)
+  
+  function handleUpdate(){
+    const user = {
+      name, 
+      email,
+      password: passwordNew,
+      old_password: passwordOld
+    }
+ 
+    updateProfile({user, avatarFile})
+  }
+
+  function handleChangeAvatar(event) {
+    const file = event.target.files[0]
+    setAvatarFile(file)
+
+    const imagePreview = URL.createObjectURL(file)
+    setAvatar(imagePreview)
+  
+  }
   return(
     <Container>
       <header>
@@ -16,7 +51,7 @@ export function Profile(){
       <Form>
         <Avatar>
           <img 
-            src="http://github.com/danielvalmeida91.png"
+            src={avatar}
             alt="Foto do usuário"
           />
 
@@ -25,6 +60,7 @@ export function Profile(){
             <input 
               id="avatar"
               type="file"
+              onChange={handleChangeAvatar}
             />
           </label>
         </Avatar>
@@ -32,27 +68,33 @@ export function Profile(){
           placeholder="Nome"
           type="text"
           icon={FiUser}
+          value={name}
+          onChange={e => setName(e.target.value)}
         />
 
         <Input 
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          value={email}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input 
           placeholder="Senha Atual"
           type="password"
           icon={FiLock}
+          onChange={e => setPasswordOld(e.target.value)}
         />
 
         <Input 
           placeholder="Nova Senha"
           type="password"
           icon={FiLock}
+          onChange={e => setPasswordNew(e.target.value)}
         />
 
-        <Button title="Salvar"/>
+        <Button title="Salvar" onClick={handleUpdate}/>
       </Form>
     </Container>
   );
